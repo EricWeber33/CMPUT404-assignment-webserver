af0a188fa03cec5ccd7c196d7b6a52637cf729aa
Eric Weber <edweber@cs.ualberta.ca>
2022-09-26 22:31:33 -0600

    add test images, server fixes

--- edweber/server.py
+++ edweber/server.py
@@ -52,12 +52,11 @@ class MyWebServer(socketserver.BaseRequestHandler):
 
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        method, resource, location = self.parse_request(self.data)
+        method, resource = self.parse_request(self.data)
         if method == None or resource == None:
             self.add_and_complete_response_header(BAD_REQUEST)
         elif method == 'GET':
-            self.do_get(resource, location=location)  
+            self.do_get(resource)  
         elif method in ['POST', 'DELETE', 'PUT', 'PATCH', 'HEAD', 'TRACE', 'CONNECT', 'OPTIONS']:
             self.add_and_complete_response_header(METHOD_NOT_ALLOWED)
         else:
@@ -66,7 +65,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
             
 
 
-    def do_get(self, resource, location=None):
+    def do_get(self, resource):
         """
         handles the case of a get request
         param: resource (string) is a path to a requested resource
@@ -79,20 +78,17 @@ class MyWebServer(socketserver.BaseRequestHandler):
         full_path = root_dir + resource
         if exists(full_path):
             if isdir(full_path):
-                self.send_moved_response(resource, location)
+                self.send_moved_response(resource)
                 return
             with open(root_dir+resource, 'r') as f:
                 content = f.read()
             self.add_respose_header(OK)
             if resource.endswith('.html'):
                 self.response += 'Content-Type: text/html\r\n'
-                #self.request.sendall(bytearray('Content-Type: text/html\r\n', 'utf-8'))
             elif resource.endswith('.css'):
                 self.response += 'Content-Type: text/css\r\n'
-                #self.request.sendall(bytearray('Content-Type: text/css\r\n', 'utf-8'))
             self.add_newline()
             self.response += content
-            #self.request.sendall(bytearray(content, 'utf-8'))
             self.add_newline()
         else:
             self.add_and_complete_response_header(NOT_FOUND)
@@ -133,17 +129,14 @@ class MyWebServer(socketserver.BaseRequestHandler):
             data_parts = request_data.split()
             method = data_parts[0].decode('utf-8').strip()
             resource = data_parts[1].decode('utf-8').strip()
-            location = data_parts[4].decode('utf-8').strip()
-            return (method, resource, location)
+            return (method, resource)
         except IndexError:
-            return (None, None, None)
+            return (None, None)
 
-    def send_moved_response(self, resource, location):
+    def send_moved_response(self, resource):
         self.add_respose_header(MOVED)
-        link = f'http://{location+resource}/'
-        location_message = f'Location: {link}\r\n'
+        location_message = f'Location: http://127.0.0.1:8080{resource}/\r\n'
         self.response += location_message
-        #self.request.sendall(bytearray(location_message, 'utf-8'))
         self.add_newline()
 
 
@@ -155,12 +148,10 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def add_respose_header(self, code):
         response_header = f'HTTP/1.1 {code} {self.http_responses[code]}\r\n'
         self.response += response_header
-        #self.request.sendall(bytearray(response, 'utf-8'))
 
 
     def add_newline(self):
         self.response += '\r\n'
-        #self.request.sendall(bytearray('\r\n', 'utf-8'))
 
     def send_full_response(self):
         self.request.sendall(bytearray(self.response, 'utf-8'))
